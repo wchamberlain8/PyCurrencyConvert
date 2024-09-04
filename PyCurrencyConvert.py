633fb1da1b6ced971b9bbb0ea2f54ea2da3f270d
wchamberlain8 <william.chamberlain8@gmail.com>
2024-09-04 15:40:18 +0100

    Added custom conversions

--- PyCurrencyConvert.py
+++ PyCurrencyConvert.py
@@ -1,16 +1,38 @@
 import requests
+import time
+import os
 
-url = 'https://v6.exchangerate-api.com/v6/80c772aad2e7ff37519f5ec5/latest/GBP' #API key
+#'https://v6.exchangerate-api.com/v6/80c772aad2e7ff37519f5ec5/latest/CURRENCY_CODE' #API key
 
-#response = requests.get(url) #http get request
-#data = response.json() 
+
+def getConversionRate(userCurrency, targetCurrency):
+    url = f'https://v6.exchangerate-api.com/v6/80c772aad2e7ff37519f5ec5/latest/{userCurrency}' #API key
+
+    response = requests.get(url) #http get request
+    data = response.json()
+
+    if targetCurrency in data['conversion_rates']:
+        return data['conversion_rates'][targetCurrency]
+    else:
+        print(f"Error: {targetCurrency} is not available in the API")
+        return None
+
 
 def customConversion():
-    userCurrency = input("Enter currency you wish to convert: ")
-    value = input("Enter the amount of money: ")
-    targetCurrency = input("Enter desired currency: ")
 
-#print(data)
+    userCurrency = input("Enter currency you wish to convert: ").upper
+    value = float(input("Enter the amount of money: "))
+    targetCurrency = input("Enter desired currency: ").upper
+
+    conversionRate = getConversionRate(userCurrency, targetCurrency)
+
+    if conversionRate:
+        newValue = value * conversionRate
+        print(f"{value} {userCurrency} is equivalent to {newValue} {targetCurrency}")
+
+    
+
+
 
 def menu():
     print ("---------------------------------------")
@@ -28,13 +50,32 @@ def menu():
     print ("---------------------------------------")
 
 def menuInput():
-    lock = True
-    while lock == True:
+    while True:
         userInput = input("Please enter your selection [1-7]: ")
 
         if userInput == '1':
-            lock = False
+            os.system('cls')
             customConversion()
+            return
+        elif userInput == '2':
+            # function call
+            return
+        elif userInput == '3':
+            # function call
+            return
+        elif userInput == '4':
+            # function call
+            return
+        elif userInput == '5':
+            # function call
+            return
+        elif userInput == '6':
+            # function call
+            return
+        elif userInput == '7':
+            print("Exiting...")
+            time.sleep(1)
+            exit()
         else:
             print("Invalid choice, try again")
 
